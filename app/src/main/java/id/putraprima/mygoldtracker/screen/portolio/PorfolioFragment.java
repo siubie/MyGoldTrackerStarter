108bc1f8387ff518f9c0bb1439c722d3cb3203c9
Putra Prima Arhandi <putraprima@gmail.com>
2020-12-02 07:53:16 +0700

    Starter Code

---
+++ app/src/main/java/id/putraprima/mygoldtracker/screen/portolio/PorfolioFragment.java
@@ -0,0 +1,31 @@
+package id.putraprima.mygoldtracker.screen.portolio;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.putraprima.mygoldtracker.R;
+
+public class PorfolioFragment extends Fragment {
+
+
+    public PorfolioFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_porfolio, container, false);
+    }
+}
